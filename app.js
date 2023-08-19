60a3f15aa480dbf51a220ded675be36c0cd5f7d5
LanceFontanilla <lancefontanilla@gmail.com>
2023-08-19 14:13:59 -0600

    working on styling

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let clickUpgrades = [
     {
         name: 'mallet',
-        price: 250,
+        price: 100,
         quantity: 0,
         multiplier: 5,
         totalDmg: 0
@@ -33,29 +33,17 @@ let automaticUpgrades = [
 ]
 
 let moleTotal = 0
-let molePerClick = 0
 let molePerSecond = 0
-
-
-
-
-
-
 
 function mine() {
     moleTotal++
     console.log('this is mining', moleTotal)
     totalClickDmg()
     drawTotals()
-    //applyClickUpgrade()
 }
 
 function drawTotals() {
     document.getElementById('moleTotal').innerText = `Total Moles: ${moleTotal}`
-
-
-
-    drawAutomaticUpgradePrice()
 }
 
 function buyClickUpgrades(clickUpgradeName) {
@@ -65,17 +53,13 @@ function buyClickUpgrades(clickUpgradeName) {
         purchasedClickUpgrade.totalDmg = (purchasedClickUpgrade.multiplier * purchasedClickUpgrade.quantity)
         moleTotal -= purchasedClickUpgrade.price
         purchasedClickUpgrade.price += 100
-
     } else if (moleTotal < purchasedClickUpgrade.price) {
         window.alert("You don't have enough moles!!")
     }
-    // totalClickDmg()
     drawTotals()
     drawClickUpgrade()
     drawClickUpgradePrice()
 }
-
-
 
 function buyAutomaticUpgrades(automaticUpgradeName) {
     let purchasedAutomaticUpgrade = automaticUpgrades.find(automaticUpgrade => automaticUpgrade.name == automaticUpgradeName)
@@ -84,14 +68,13 @@ function buyAutomaticUpgrades(automaticUpgradeName) {
         purchasedAutomaticUpgrade.totalDmg = (purchasedAutomaticUpgrade.multiplier * purchasedAutomaticUpgrade.quantity)
         moleTotal -= purchasedAutomaticUpgrade.price
         purchasedAutomaticUpgrade.price += 500
-
     } else if (moleTotal < purchasedAutomaticUpgrade.price) {
         window.alert("You don't have enough moles!!")
     }
     drawTotals()
     drawAutomaticUpgrade()
+    drawAutomaticUpgradePrice()
 }
-
 
 function drawAutomaticUpgradePrice() {
     let automaticUpgradePriceTemplate = ''
@@ -101,7 +84,6 @@ function drawAutomaticUpgradePrice() {
             <p>${automaticUpgrade.name} Price: ${automaticUpgrade.price} Moles</p>
             </div>
         `
-
     })
     document.getElementById('automaticUpdatePrice').innerHTML = automaticUpgradePriceTemplate
 }
@@ -114,7 +96,6 @@ function drawClickUpgradePrice() {
             <p>${clickUpgrade.name} Price: ${clickUpgrade.price} Moles</p>
             </div>
         `
-
     })
     document.getElementById('clickUpdatePrice').innerHTML = clickUpgradePriceTemplate
 }
@@ -134,7 +115,6 @@ function drawClickUpgrade() {
         }
     })
     document.getElementById('clickUpgrade').innerHTML = clickUpgradeTemplate
-
     drawTotals()
 }
 
@@ -151,10 +131,10 @@ function drawAutomaticUpgrade() {
             </div>
             `
         }
+        document.getElementById('automaticUpgrade').innerHTML = automaticUpgradeTemplate
+        drawTotals()
+        document.getElementById('molePerSecond').innerText = `Moles Per Second: ${molePerSecond}`
     })
-    document.getElementById('automaticUpgrade').innerHTML = automaticUpgradeTemplate
-    drawTotals()
-    document.getElementById('molePerSecond').innerText = `Moles Per Second: ${molePerSecond}`
 }
 
 function collectAutomaticUpgrades() {
@@ -164,15 +144,26 @@ function collectAutomaticUpgrades() {
         //console.log('this is the total autoUpgrades', totalAutomaticUpgrades)
         drawTotals()
     })
-
 }
 
 function totalClickDmg() {
     clickUpgrades.forEach(clickUpgrade => {
-        let molePerClick = (clickUpgrade.quantity * clickUpgrade.multiplier)
-        moleTotal += molePerClick
+        if (clickUpgrade.quantity > 0) {
+            let molePerClick = (clickUpgrade.quantity * clickUpgrade.multiplier)
+            moleTotal += molePerClick
+            document.getElementById('molePerClick').innerText = `Moles Per Click: ${molePerClick}`
+        }
     })
-    document.getElementById('molePerClick').innerText = `Moles Per Click: ${molePerClick}`
+}
+
+function debugTool() {
+    moleTotal += 2500
+    drawTotals()
+}
+
+function debugToolZero() {
+    moleTotal = 0
+    drawTotals()
 }
 
 setInterval(collectAutomaticUpgrades, 3000);
